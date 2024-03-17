2c027b741e42bcedcfc03670280fbe2488753f8b
MitulMistry <MitulMistryDev@gmail.com>
2024-03-17 17:22:48 -0500

    Add Storybook file for Selector component

---
+++ features/ui/selector/selector.stories.tsx
@@ -0,0 +1,31 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Selector, SelectorSize } from ".";
+
+// Storybook CSF3 format
+
+const meta: Meta<typeof Selector> = {
+  title: "UI/Selector",
+  component: Selector,
+};
+export default meta;
+
+type Story = StoryObj<typeof Selector>;
+
+export const Default: Story = {
+  args: { children: "Selector", isDisabled: false },
+};
+
+export const Small: Story = {
+  ...Default,
+  args: { ...Default.args, size: SelectorSize.Small },
+};
+
+export const Medium: Story = {
+  ...Default,
+  args: { ...Default.args, size: SelectorSize.Medium },
+};
+
+export const Large: Story = {
+  ...Default,
+  args: { ...Default.args, size: SelectorSize.Large },
+};
