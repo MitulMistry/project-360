7365a03fe8370c4359c5a711ad8bd1760cdc251d
Mitul Mistry <MitulMistry@users.noreply.github.com>
2024-03-18 20:10:31 -0500

    UI kit status button (#12)
    * Create StatusButton component
    
    * Add Storybook file for StatusButton component
    
    * Add different color styles for StatusButton

---
+++ features/ui/status-button/status-button.stories.tsx
@@ -0,0 +1,41 @@
+import { Meta, StoryObj } from "@storybook/react";
+import {
+  StatusButton,
+  StatusButtonSize,
+  statusColors,
+  priorityColors,
+} from ".";
+
+// Storybook CSF3 format
+
+const meta: Meta<typeof StatusButton> = {
+  title: "UI/StatusButton",
+  component: StatusButton,
+};
+export default meta;
+
+type Story = StoryObj<typeof StatusButton>;
+
+export const Default: Story = {
+  args: { items: statusColors, isDisabled: false },
+};
+
+export const Small: Story = {
+  ...Default,
+  args: { ...Default.args, size: StatusButtonSize.Small },
+};
+
+export const Medium: Story = {
+  ...Default,
+  args: { ...Default.args, size: StatusButtonSize.Medium },
+};
+
+export const Large: Story = {
+  ...Default,
+  args: { ...Default.args, size: StatusButtonSize.Large },
+};
+
+export const Priority: Story = {
+  ...Default,
+  args: { ...Default.args, items: priorityColors },
+};
---
+++ features/ui/status-button/status-button.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import React, { useState } from "react";
+import type { ButtonProps as AriaButtonProps } from "react-aria-components";
+import { Button as AriaButton } from "react-aria-components";
+import classNames from "classnames";
+import styles from "./status-button.module.scss";
+
+export enum StatusButtonSize {
+  Small = "sm",
+  Medium = "md",
+  Large = "lg",
+}
+
+export enum StatusColor {
+  Ready = "ready",
+  InProgress = "inProgress",
+  Done = "done",
+  Stuck = "stuck",
+  Low = "low",
+  Medium = "medium",
+  Critical = "critical",
+  High = "high",
+}
+
+type StatusColorItem = {
+  name: string;
+  color: StatusColor;
+};
+
+export const statusColors = [
+  { name: "Ready to Start", color: StatusColor.Ready },
+  { name: "In Progress", color: StatusColor.InProgress },
+  { name: "Done", color: StatusColor.Done },
+  { name: "Stuck", color: StatusColor.Stuck },
+];
+
+export const priorityColors = [
+  { name: "Low", color: StatusColor.Low },
+  { name: "Medium", color: StatusColor.Medium },
+  { name: "Critical", color: StatusColor.Critical },
+  { name: "High", color: StatusColor.High },
+];
+
+// React-Aria Button component is not native HTML Button element,
+// so doesn't make sense to extend it.
+// https://react-spectrum.adobe.com/react-aria/Button.html#props
+type StatusButtonProps = AriaButtonProps & {
+  className?: string;
+  size?: StatusButtonSize;
+  initializedId?: number;
+  items: StatusColorItem[];
+};
+
+export function StatusButton({
+  className,
+  size = StatusButtonSize.Medium,
+  initializedId = 0,
+  items,
+  ...props
+}: StatusButtonProps) {
+  const [selectedId, setSelectedId] = useState(initializedId);
+
+  const increment = () => {
+    const newId = selectedId === items.length - 1 ? 0 : selectedId + 1;
+    setSelectedId(newId);
+  };
+
+  return (
+    <div className={classNames(styles.container, className)}>
+      <AriaButton
+        className={classNames(
+          styles.statusButton,
+          styles[size],
+          styles[items[selectedId].color],
+        )}
+        onPress={() => increment()}
+        {...props}
+      >
+        {items[selectedId].name}
+      </AriaButton>
+    </div>
+  );
+}
