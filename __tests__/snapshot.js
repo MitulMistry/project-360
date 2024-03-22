17badecf71499da39a1820046c59116b9c961144
MitulMistry <MitulMistryDev@gmail.com>
2024-03-22 13:43:29 -0500

    Remove Jest splash page tests to skip testing async server component

--- __tests__/snapshot.js
+++
@@ -1,7 +0,0 @@
-import { render } from "@testing-library/react";
-import Page from "../app/page";
-
-it("renders splash page unchanged", () => {
-  const { container } = render(<Page />);
-  expect(container).toMatchSnapshot();
-});
