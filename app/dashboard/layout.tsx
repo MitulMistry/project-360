558c3663f4328f2456674554463455356f63be14
MitulMistry <MitulMistryDev@gmail.com>
2024-03-27 00:11:22 -0500

    Set up unauthenticated redirect for dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,11 +1,21 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/lib/authOptions";
+import { redirect } from "next/navigation";
+import { Routes } from "@config/routes";
 import { SidebarNavigation } from "@features/layout";
 import styles from "./layout.module.scss";
 
-export default function DashboardLayout({
+// Asynchronous server component to access session
+export default async function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // If user is not authenticated, should not have access to any dashboard
+  // pages, so redirect to login
+  const session = await getServerSession(authOptions);
+  if (!session) redirect(Routes.login);
+
   return (
     <div className={styles.container}>
       <SidebarNavigation className={styles.navigation} />
