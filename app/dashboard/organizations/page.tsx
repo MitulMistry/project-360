4854d88a8332b89ee9446d736516ddacdc64170d
MitulMistry <MitulMistryDev@gmail.com>
2024-03-30 00:14:44 -0500

    Set up structure of Organizations page

--- app/dashboard/organizations/page.tsx
+++ app/dashboard/organizations/page.tsx
@@ -1,10 +1,36 @@
+"use client";
+
 import { PageContainer } from "@features/layout";
-// import styles from "./page.module.scss";
+import { Button, ButtonColor, ButtonSize } from "@features/ui";
+import { OrganizationList } from "@features/organizations";
+import styles from "./page.module.scss";
 
 export default function Organizations() {
   return (
     <PageContainer title="Organizations">
-      <h1>Organizations</h1>
+      <div className={styles.container}>
+        <div className={styles.headerRow}>
+          <h1>Organizations</h1>
+          <div className={styles.buttons}>
+            <Button
+              size={ButtonSize.Medium}
+              color={ButtonColor.White}
+              className={styles.button}
+            >
+              Join Organization
+            </Button>
+            <Button
+              size={ButtonSize.Medium}
+              color={ButtonColor.Primary}
+              className={styles.button}
+            >
+              New Organization
+            </Button>
+          </div>
+        </div>
+
+        <OrganizationList />
+      </div>
     </PageContainer>
   );
 }
