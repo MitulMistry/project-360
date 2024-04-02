2b4f08514babb3c2904a53e586ec408ad9e43306
MitulMistry <MitulMistryDev@gmail.com>
2024-04-02 00:11:12 -0500

    Enable buttons to show and hide organization forms

--- app/dashboard/organizations/organizations-interface.tsx
+++ app/dashboard/organizations/organizations-interface.tsx
@@ -1,10 +1,28 @@
 "use client";
 
+import { useState } from "react";
 import { Button, ButtonColor, ButtonSize } from "@features/ui";
 import { PlusIcon } from "@features/ui";
 import styles from "./organizations-interface.module.scss";
+import {
+  OrganizationJoinForm,
+  OrganizationNewForm,
+} from "@/features/organizations";
 
 export default function OrganizationsInterface() {
+  const [showJoinForm, setShowJoinForm] = useState(false);
+  const [showNewForm, setShowNewForm] = useState(false);
+
+  const enableJoinForm = () => {
+    setShowJoinForm(true);
+    setShowNewForm(false);
+  };
+
+  const enableNewForm = () => {
+    setShowJoinForm(false);
+    setShowNewForm(true);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.headerRow}>
@@ -14,6 +32,8 @@ export default function OrganizationsInterface() {
             size={ButtonSize.Medium}
             color={ButtonColor.White}
             className={styles.button}
+            onPress={enableJoinForm}
+            isDisabled={showJoinForm}
           >
             Join Organization
           </Button>
@@ -21,12 +41,16 @@ export default function OrganizationsInterface() {
             size={ButtonSize.Medium}
             color={ButtonColor.Primary}
             className={styles.button}
+            onPress={enableNewForm}
+            isDisabled={showNewForm}
           >
             <PlusIcon />
             New Organization
           </Button>
         </div>
       </div>
+      {showJoinForm && <OrganizationJoinForm className={styles.form} />}
+      {showNewForm && <OrganizationNewForm className={styles.form} />}
     </div>
   );
 }
