3625665ff896560675236e7ec0e90c6493af3dbe
MitulMistry <MitulMistryDev@gmail.com>
2024-04-08 16:45:42 -0500

    Add error notification to organizations page

--- app/dashboard/organizations/organizations-page.tsx
+++ app/dashboard/organizations/organizations-page.tsx
@@ -6,16 +6,22 @@ import { useGetOrganizations } from "./api/use-get-organizations";
 import {
   LoadingIndicator,
   LoadingIndicatorSize,
-} from "@/features/ui/loading-indicator";
+  Notification,
+  NotificationColor,
+} from "@features/ui";
 import styles from "./organizations-page.module.scss";
 
 export default function OrganizationsPage() {
-  // const { isPending, isError, data, error } = useGetOrganizations();
-  const { isPending, data } = useGetOrganizations();
+  const { isPending, isError, data, error } = useGetOrganizations();
 
   return (
     <>
       <OrganizationsInterface />
+      {isError && (
+        <Notification color={NotificationColor.ErrorSolid}>
+          Error: {error.message}
+        </Notification>
+      )}
       {isPending ? (
         <LoadingIndicator
           size={LoadingIndicatorSize.Large}
