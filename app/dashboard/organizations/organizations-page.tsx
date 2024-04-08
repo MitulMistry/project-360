6414903938f5f6275925a7f670f7cb911b64a111
MitulMistry <MitulMistryDev@gmail.com>
2024-04-08 15:46:41 -0500

    Add loading indicator to organizations page

--- app/dashboard/organizations/organizations-page.tsx
+++ app/dashboard/organizations/organizations-page.tsx
@@ -3,15 +3,27 @@
 import { OrganizationList } from "@features/organizations";
 import OrganizationsInterface from "./organizations-interface";
 import { useGetOrganizations } from "./api/use-get-organizations";
+import {
+  LoadingIndicator,
+  LoadingIndicatorSize,
+} from "@/features/ui/loading-indicator";
+import styles from "./organizations-page.module.scss";
 
 export default function OrganizationsPage() {
   // const { isPending, isError, data, error } = useGetOrganizations();
-  const { data } = useGetOrganizations();
+  const { isPending, data } = useGetOrganizations();
 
   return (
     <>
       <OrganizationsInterface />
-      <OrganizationList organizations={data} />
+      {isPending ? (
+        <LoadingIndicator
+          size={LoadingIndicatorSize.Large}
+          className={styles.loadingIndicator}
+        />
+      ) : (
+        <OrganizationList organizations={data} />
+      )}
     </>
   );
 }
