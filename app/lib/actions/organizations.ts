0786d863886286308d667d4e190462f952b81a52
MitulMistry <MitulMistryDev@gmail.com>
2024-04-08 22:17:11 -0500

    Add additional CRUD server actions for organizations

--- app/lib/actions/organizations.ts
+++ app/lib/actions/organizations.ts
@@ -1,6 +1,6 @@
 import prisma from "@/lib/prisma";
 import { z } from "zod";
-import type { User, Organization } from "@prisma/client";
+import type { User, Organization, OrganizationUser } from "@prisma/client";
 import { Role } from "@prisma/client";
 
 const FormSchema = z.object({
@@ -86,3 +86,158 @@ export async function createOrganization(
     throw new Error("Failed to create the organization.");
   }
 }
+
+const JoinOrganization = FormSchema.omit({ name: true });
+
+export async function joinOrganization(
+  formData: Omit<Organization, "name" | "createdAt"> & { userEmail: string },
+) {
+  // Validate form using Zod
+  const validatedFields = JoinOrganization.safeParse({
+    id: formData.id,
+    userEmail: formData.userEmail,
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Join Organization.",
+    };
+  }
+
+  const { id, userEmail } = validatedFields.data;
+
+  try {
+    const user: User | null = await prisma.user.findUnique({
+      where: { email: userEmail },
+    });
+
+    const organization: Organization | null =
+      await prisma.organization.findUnique({
+        where: { id: id },
+      });
+
+    if (user && organization) {
+      await prisma.organizationUser.create({
+        data: {
+          userId: user?.id,
+          organizationId: organization?.id,
+          role: Role.ASSIGNEE,
+        },
+      });
+    } else {
+      throw new Error("Failed to create organizationUser.");
+    }
+
+    return organization;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to join the organization.");
+  }
+}
+
+const UpdateOrganization = FormSchema;
+
+export async function updateOrganization(
+  formData: Omit<Organization, "createdAt"> & { userEmail: string },
+) {
+  // Validate form using Zod
+  const validatedFields = UpdateOrganization.safeParse({
+    id: formData.id,
+    name: formData.name,
+    userEmail: formData.userEmail,
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Update Organization.",
+    };
+  }
+
+  const { id, name, userEmail } = validatedFields.data;
+
+  try {
+    const user: User | null = await prisma.user.findUnique({
+      where: { email: userEmail },
+    });
+
+    const organizationUser: OrganizationUser | null =
+      await prisma.organizationUser.findFirstOrThrow({
+        where: { userId: user?.id, organizationId: id },
+      });
+
+    // Only update the organization if user is an owner
+    let organization;
+    if (organizationUser.role === Role.OWNER) {
+      organization = await prisma.organization.update({
+        where: {
+          id: id,
+        },
+        data: {
+          name: name,
+        },
+      });
+    } else {
+      throw new Error("Failed to update organization.");
+    }
+
+    return organization;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to update the organization.");
+  }
+}
+
+const DeleteOrganization = FormSchema.omit({ name: true });
+
+export async function deleteOrganization(
+  formData: Omit<Organization, "name" | "createdAt"> & { userEmail: string },
+) {
+  // Validate form using Zod
+  const validatedFields = DeleteOrganization.safeParse({
+    id: formData.id,
+    userEmail: formData.userEmail,
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Delete Organization.",
+    };
+  }
+
+  const { id, userEmail } = validatedFields.data;
+
+  try {
+    const user: User | null = await prisma.user.findUnique({
+      where: { email: userEmail },
+    });
+
+    const organizationUser: OrganizationUser | null =
+      await prisma.organizationUser.findFirstOrThrow({
+        where: { userId: user?.id, organizationId: id },
+      });
+
+    // Only delete the organization if user is an owner
+    if (organizationUser.role === Role.OWNER) {
+      await prisma.organization.delete({
+        where: {
+          id: id,
+        },
+      });
+    } else {
+      throw new Error("Failed to delete organization.");
+    }
+
+    return null;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to delete the organization.");
+  }
+}
+
+// const findUser = async (email: string): Promise<User | null> =>
+//   prisma.user.findUnique({
+//     where: { email: email },
+//   });
